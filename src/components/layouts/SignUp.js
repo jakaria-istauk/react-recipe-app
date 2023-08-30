fd2a9acbcca3bb6903823d5ec07928eae4a57e6e
Jakaria Istauk <jakariamd35@gmail.com>
2023-08-30 22:52:37 +0600

    trying to register user......

--- src/components/layouts/SignUp.js
+++ src/components/layouts/SignUp.js
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-function SignUp() {
+const SignUp = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        
+        const user = {
+            name: formData.get('firstName') + formData.get('lastName'),
+			email: formData.get('email'),
+			password: formData.get('password'),
+		}
+        console.log(user);
+    }
+
   return (
     <div className='container'>
-        <form action="#" method='post'>
+        <form action="#" method='post' onSubmit={handleSubmit}>
             <div className='row justify-content-md-center'>
                 <div className='card col col-lg-6 m-4 p-4'>
                     <div className='row g-2 mb-4'>
                         <div className="col-lg-6 form-outline">
                             <label className="form-label" htmlFor="firstName">First Name</label>
-                            <input type="text" id="firstName" className="form-control" />
+                            <input type="text" id="firstName" name='firstName' className="form-control" />
                         </div>
                         <div className="col-lg-6 form-outline">
                             <label className="form-label" htmlFor="lastName">Last Name</label>
-                            <input type="text" id="lastName" className="form-control" />
+                            <input type="text" id="lastName" name='lastName' className="form-control" />
                         </div>
                     </div>
                     <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="loginName">Email</label>
-                        <input type="email" id="loginName" className="form-control" />
+                        <label className="form-label" htmlFor="email">Email</label>
+                        <input type="email" id="email" name='email' className="form-control" />
                     </div>
 
                     <div className="form-outline mb-4">
                         <label className="form-label" htmlFor="loginPassword">Password</label>
-                        <input type="password" id="loginPassword" className="form-control" />
+                        <input type="password" id="loginPassword" name='password' autoComplete='' className="form-control" />
                     </div>
 
                     <button type="submit" className="btn btn-primary btn-block mb-4">Sign Up</button>
@@ -39,4 +51,4 @@ function SignUp() {
   )
 }
 
-export default SignUp
+export default SignUp;
